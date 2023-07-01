6b1fc3c6bbf04be81b4d06f53d34eb9b037082e1
Satish Venna <satishkumar.venna@gmail.com>
2023-07-01 14:05:10 -0400

    added initial project code to track changes

---
+++ src/components/home/Timer.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const savedSeconds = parseInt(sessionStorage.getItem("timerSeconds"));
+    if (!isNaN(savedSeconds)) {
+      setSeconds(savedSeconds);
+    }
+
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        const updatedSeconds = prevSeconds + 1;
+        sessionStorage.setItem("timerSeconds", updatedSeconds.toString());
+        return updatedSeconds;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600)
+      .toString()
+      .padStart(2, "0");
+    const minutes = Math.floor((time % 3600) / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  return (
+    <div>
+      <h2>Timer:</h2>
+      <p>{formatTime(seconds)}</p>
+    </div>
+  );
+};
+
+export default Timer;
